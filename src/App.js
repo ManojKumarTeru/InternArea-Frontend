91c45fbaa5273e9601ffef26d4db7eedf7eb4628
Delta Student <someone@email.com>
2024-06-28 17:13:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import { Routes,Route } from 'react-router-dom';
+import Footer from './components/Footer/footer';
+import Register from './components/auth/Register'
+import Intern from './components/Internships/Intern'
+import JobAvl from './components/Job/JobAvl'
+import JobDetail from './components/Job/JobDetail';
+import InternDeatil from './components/Internships/InternDeatil';
+import { useDispatch} from 'react-redux';
+import { login,logout} from "./Feature/Userslice"
+import { auth } from './firebase/firebase';
+import { useEffect } from 'react';
+import Profile from './profile/profile';
+import AdminLogin from './Admin/AdminLogin';
+import Adminpanel from './Admin/Adminpanel';
+import Postinternships from './Admin/Postinternships';
+import ViewAllApplication from './Admin/ViewAllApplication'
+import DetailApplication from './Applications/DetailApplication';
+import UserApplicatiom from './profile/UserApplication';
+import UserDetailApplication from './Applications/DetailApplicationUser';
 function App() {
+
+  const dispatch=useDispatch();
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser)=>{
+      
+      if(authUser){
+        dispatch(login({
+  
+          uid:authUser.uid,
+          photo:authUser.photoURL,
+          name:authUser.displayName,
+          email:authUser.email,
+          phoneNumber:authUser.phoneNumber
+        }))
+        
+      }
+        else{
+          dispatch(logout())
+        }
+    })
+    },[dispatch] );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Navbar/>
+
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/internship' element={<Intern/>}/>
+          <Route path='/Jobs' element={<JobAvl/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/Register' element={<Register/>}/>
+          <Route path='/detailjob' element={<JobDetail/>}/>
+          <Route path='/detailApplication' element={<DetailApplication/>}/>
+          <Route path='/detailInternship' element={<InternDeatil/>}/>
+          <Route path='/adminLogin' element={<AdminLogin/>}/>
+          <Route path='/adminepanel' element={<Adminpanel/>}/>
+          <Route path='/applications' element={<ViewAllApplication/>}/>
+          <Route path='/UserapplicationDetail' element={<UserDetailApplication/>}/>
+          <Route path='/userapplication' element={<UserApplicatiom/>}/>
+      </Routes>
+      <Footer />
+    </div> 
   );
 }
 
 export default App;
+  
---
+++ src/firebase/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getAuth,GoogleAuthProvider}from 'firebase/auth'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDix8WabNOBCbvgt2fkZe0gqvGHsHde06o",
+  authDomain: "internarea-c010d.firebaseapp.com",
+  projectId: "internarea-c010d",
+  storageBucket: "internarea-c010d.appspot.com",
+  messagingSenderId: "178299575103",
+  appId: "1:178299575103:web:6dfab577a0bc8fbb96d18a",
+  measurementId: "G-TBSJT5PBXK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth=getAuth(app);
+const provider=new GoogleAuthProvider();
+export {auth,provider}
